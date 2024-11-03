e0d5ddf4947c4edfa5d15da0d70d732e3cf8b70b
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-11-03 18:09:33 +0100

    remove unused code

--- core/util/merge_batch.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import "github.com/jackc/pgx/v5"
-
-func MergeBatch(from pgx.Batch, to *pgx.Batch) {
-	for _, q := range from.QueuedQueries {
-		to.Queue(q.SQL, q.Arguments...)
-	}
-}
